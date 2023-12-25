20c53b6189260c96084a46cc9ac0db7d940d7f8c
cyx <yanxin152133@gmail.com>
2023-12-26 00:07:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [
         vue(),
     ],
-    base: './'
+    base: './',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
