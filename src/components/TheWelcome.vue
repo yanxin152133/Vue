b27ec30187cd284202e53801fcc91366240ce3d0
cyx <yanxin152133@gmail.com>
2024-03-12 16:56:13 +0800

    Update TheWelcome.vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -44,4 +44,14 @@ import DocumentationIcon from './icons/IconDocumentation.vue'
     Vue 表单输入绑定
   </WelcomeItem>
 
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon/>
+    </template>
+    <template #heading>
+      <RouterLink to="/FormInputBindings">test jenkins</RouterLink>
+    </template>
+    Vue test jenkins
+  </WelcomeItem>
+
 </template>
