8678d6739268355f913a061df974770989b4d7bd
yanxin152133 <yanxin152133@gmail.com>
2024-04-23 15:53:19 +0800

    reformat code

--- src/views/ConditionalRendering.vue
+++ src/views/ConditionalRendering.vue
@@ -1,26 +1,26 @@
 <template>
-    <main>
-        <h1>条件渲染</h1>
+  <main>
+    <h1>条件渲染</h1>
 
-        <h2 v-if="flag">v-if 语法</h2>
-        <h2 v-else>v-else 语法</h2>
+    <h2 v-if="flag">v-if 语法</h2>
+    <h2 v-else>v-else 语法</h2>
 
-        <h2 v-if="type === 'A'">A</h2>
-        <h2 v-else-if="type === 'B'">B</h2>
-        <h2 v-else-if="type === 'C'">C</h2>
-        <h2 v-else> not A B C</h2>
+    <h2 v-if="type === 'A'">A</h2>
+    <h2 v-else-if="type === 'B'">B</h2>
+    <h2 v-else-if="type === 'C'">C</h2>
+    <h2 v-else> not A B C</h2>
 
-        <h2 v-show="flag">v-show 语法</h2>
-        
-    </main>
+    <h2 v-show="flag">v-show 语法</h2>
+
+  </main>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            flag: false,
-            type: "C"
-        }
+  data() {
+    return {
+      flag: false,
+      type: "C"
     }
+  }
 }
 </script>
