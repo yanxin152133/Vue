a81c890c53ce0a67cc1c37eb4092249063e1b28f
yanxin152133 <yanxin152133@gmail.com>
2025-01-06 14:07:37 +0800

    docs(模板引用):

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue' // 首页
 import TemplateSyntax from '../views/TemplateSyntax.vue' // 模板语法
 import ConditionalRendering from '../views/ConditionalRendering.vue' // 条件渲染
@@ -6,6 +6,7 @@ import ListRendering from '../views/ListRendering.vue' //列表渲染
 import FormInputBindings from "../views/FormInputBindings.vue"  //表单输入绑定
 import EventHandling from "@/views/EventHandling.vue";    //事件处理
 import Watchers from "@/views/Watchers.vue";    //侦听器
+import TemplateRefs from "@/views/TemplateRefs.vue";    //模板引用
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +45,11 @@ const router = createRouter({
             path: '/Watchers',
             name: 'Watchers',
             component: Watchers
+        },
+        {
+            path: '/TemplateRefs',
+            name: 'TemplateRefs',
+            component: TemplateRefs
         }
 
     ]
---
+++ src/views/TemplateRefs.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {ref, useTemplateRef, onMounted, reactive} from 'vue'
+
+import Child from './ListRendering.vue'
+
+const childRef = useTemplateRef('child')
+
+// 第一个参数必须与模板中的 ref 值匹配
+const input = useTemplateRef('my-input')
+const list = ref([
+  'How to do lists in Vue',
+  'Jane Doe',
+  '2016-04-10'
+])
+
+const itemRefs = useTemplateRef('items')
+
+onMounted(() => {
+      input.value.focus()
+      console.log(itemRefs.value)
+    }
+)
+</script>
+
+<template>
+  <main>
+    <div>
+      <!--访问模板引用-->
+      <input ref="my-input"/>
+    </div>
+
+    <div>
+      <!--v-for 中的模板引用-->
+      <ul>
+        <li v-for="item in list" ref="items">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <!--组件上的ref-->
+      <Child ref="child" />
+    </div>
+
+  </main>
+</template>
